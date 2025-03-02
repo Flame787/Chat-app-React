0ef98935d9758094b7d1ff6991ff618ae5f2c518
Flame787 <mbrezovic77@gmail.com>
2025-03-02 20:49:05 +0100

    new structure, connects, not showing members

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,45 +1,63 @@
 import { useState, useEffect } from "react";
 
-import TypingIndicator from "./TypingIndicator";
+export default function Input({ sendMessage }) {
+  const placeholder = [
+    "Enter your message...",
+    "You need to type something first..."
+  ];
 
-let typingIndicator = null;
+  const initialInput = {
+    text: "",
+    placeholder: placeholder[0]
+  };
 
-export default function Input({ onsendMessage, onChangeTypingState }) {
-  const [text, setText] = useState();
 
-  // useEffect for typing-indicator:
-  useEffect(() => {
-    if (typingIndicator === null) {
-      typingIndicator = new TypingIndicator(); // creating new typingIndicator-object
-      typingIndicator.listen(onChangeTypingState);
-      // adding listener to the typing object, which activates function 'onChangeTypingState'
-    }
-  });
+  // states:
 
-  function onChange(event) {
-    const text = event.target.value;
-    typingIndicator.onChange(text);
-    setText(text);
+  const [input, setInput] = useState(initialInput);
+
+  let nameInput;
+
+  function updateInput(e) {
+    setInput({ ...input, text: e.target.value });
   }
 
-  function onSubmit(event) {
-    event.preventDefault();
-    setText("");   // clear previous message from input-field, once it was submitted
-    onsendMessage(text);
+  function publishInput(e) {
+    e.preventDefault();
+    if (input.text === "") {
+      setInput({
+        ...input,
+        placeholder: placeholder[1]
+      });
+    } else {
+      sendMessage(input.text);
+      setInput({ text: "", placeholder: placeholder[0] });
+    }
   }
 
   return (
     <div>
-      <form onSubmit={(event) => onSubmit(event)}>
-        <input
-          onChange={(event) => onChange(event)}
-          value={text}
+      <form onSubmit={publishInput}>
+      <input
+          className="msg-form__input"
+          value={input.text}
           type="text"
-          placeholder="Enter your message and press Send"
-          autoFocus
+          placeholder={input.placeholder}
+          ref={(input) => {
+            nameInput = input;
+          }}
+          autoFocus={true}
+          onChange={updateInput}
+
         />
-        <button>Send</button>
+      <button
+          type="button"
+          onMouseDown={publishInput}
+        >
+          Send
+        </button>
       </form>
     </div>
-  );
+  )
+
 }
