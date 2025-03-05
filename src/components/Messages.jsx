b14241f3b7775dcf2eb8dc0be6df166145290a91
Flame787 <mbrezovic77@gmail.com>
2025-03-05 17:24:45 +0100

    member publishing message is correctly shown

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,12 +1,16 @@
 import { useState, useEffect, useRef } from "react";
 
 export default function Messages({ messages, thisMember }) {
+//thisMember - the person from whose perspective we are currently seing the chat (user-perspective)
+
   let sameMember = "";
   // if continuos messages from same user, we don't show his username & avatar for each message
+
   const bottomDiv = useRef();
-  
+  // empty div under all messages - when new message arrives, the page scrolls to the last message (= to this div)
+
   useEffect(() => {
-    bottomDiv.current.scrollIntoView();
+    bottomDiv.current.scrollIntoView(); // auto-scrolling to the last div, whenever messages-list changes
   }, [messages.length]);
 
   function showMessage(message) {
@@ -18,6 +22,12 @@ export default function Messages({ messages, thisMember }) {
       ? (listItem = (
           <li key={id} data-id={member.id}>
             <div>
+            {/* <img
+                  src={member.clientData.avatar}
+                  alt="user-avatar"
+                /> */}
+              <div>{member.clientData.username}</div>
+              
               <div>{data}</div>
               <div></div>
             </div>
