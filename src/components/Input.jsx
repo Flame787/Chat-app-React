3dc979fc9115c6a0129edc8ea37e8c50b0fb5553
Flame787 <mbrezovic77@gmail.com>
2025-03-27 21:13:14 +0100

    edited style & added gif & file buttons

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
 import { faFaceSmile } from "@fortawesome/free-solid-svg-icons";
+import { faFilm } from "@fortawesome/free-solid-svg-icons";
+import { faFolderOpen } from "@fortawesome/free-regular-svg-icons";
 
 export default function Input({ sendMessage, thisMember }) {
   // thisMember - me / the person from whose perspective we are currently seing the chat (user-perspective)
@@ -60,7 +62,7 @@ export default function Input({ sendMessage, thisMember }) {
           <div className="input-name">{`${thisMember.username} (me)`}</div>
         </div>
         <div className="message-input">
-          <input
+          <textarea
             className="input-text"
             value={input.text}
             type="text"
@@ -83,9 +85,17 @@ export default function Input({ sendMessage, thisMember }) {
         ></input> */}
 
           <div className="buttons">
-            <button className="input-button smiley-button" type="button">
-              <FontAwesomeIcon icon={faFaceSmile} />
-            </button>
+            <div left-buttons>
+              <button className="input-button smiley-button" type="button">
+                <FontAwesomeIcon icon={faFaceSmile} /> emoji
+              </button>
+              <button className="input-button file-button" type="button">
+                <FontAwesomeIcon icon={faFilm} /> gif
+              </button>
+              <button className="input-button file-button" type="button">
+                <FontAwesomeIcon icon={faFolderOpen} /> file/img
+              </button>
+            </div>
             <button
               className="input-button send-button"
               type="button"
